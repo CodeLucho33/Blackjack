cd889b25982084ff80fc038da24d931acd2515c1
CodeLucho33 <code.lucho33@gmail.com>
2024-07-10 11:46:21 -0500

    Start withit logic implemetation a practice witcho new branch

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 // 1. Create two variables, firstCard and secondCard. 
 
-let firstCard = 7;
-let secondCard = 3;
+//let firstCard = 7;
+//let secondCard = 3;
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
-let sum = firstCard + secondCard
+//let sum = firstCard + secondCard
+//if (sum< 21){
+//console.log('Do you want to draw a new card?')
+//} else if (sum === 21){
+ //   console.log("Wohoo! You'got BLACKJACK")
+//} else if (sum>21){
+  //  console.log("You're out of the game!")
+//}
+
+/*
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 22
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if(age>21){
+      console.log("Welcome!")
+} else{
+  
+    console.log("You can not enter the club!")
+}
+
+
+
+IF/ELSE...IF/ELSE STATEMENT
+
+
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let age = 145
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+if (age < 100) {
+    console.log("Not elegible")
+} else if (age === 100) {
+    console.log("Here is your birthday card from the King!")
+} else {
+    console.log("Not elegible, you have already gotten one")
+}
+
+*/
